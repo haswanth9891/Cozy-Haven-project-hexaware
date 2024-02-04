2c0fc9ee128681ffe6fc9c8e445d9db6d5eef4c2
haswanth9891 <19bcs071@iiit.ac.in>
2024-02-04 19:25:31 +0530

    CONTROLLER WITH WORKING

--- Team 3 Project/cozyhaven/src/main/java/com/hexaware/ccozyhaven/restcontroller/AdministratorController.java
+++ Team 3 Project/cozyhaven/src/main/java/com/hexaware/ccozyhaven/restcontroller/AdministratorController.java
@@ -1,5 +1,63 @@
 package com.hexaware.ccozyhaven.restcontroller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hexaware.ccozyhaven.entities.HotelOwner;
+import com.hexaware.ccozyhaven.entities.User;
+import com.hexaware.ccozyhaven.exceptions.InvalidCancellationException;
+import com.hexaware.ccozyhaven.exceptions.ReservationNotFoundException;
+import com.hexaware.ccozyhaven.exceptions.UserNotFoundException;
+import com.hexaware.ccozyhaven.service.IAdministratorService;
+
+@RestController
+@RequestMapping("/cozyhaven-admin")
 public class AdministratorController {
+	
+	 @Autowired
+	    private IAdministratorService administratorService;
+	 
+	 @DeleteMapping("/deleteUserAccount/{userId}")
+	    public ResponseEntity<String> deleteUserAccount(@PathVariable Long userId) throws UserNotFoundException {
+	        administratorService.deleteUserAccount(userId);
+	        return new ResponseEntity<>("User account deleted successfully", HttpStatus.OK);
+	    }
+	 
+	 
+	 @DeleteMapping("/deleteHotelOwnerAccount/{hotelOwnerId}")
+	    public ResponseEntity<String> deleteHotelOwnerAccount(@PathVariable Long hotelOwnerId) throws UserNotFoundException {
+	        administratorService.deleteHotelOwnerAccount(hotelOwnerId);
+	        return new ResponseEntity<>("Hotel owner account deleted successfully", HttpStatus.OK);
+	    }
+	 
+	 @GetMapping("/viewAllUsers")
+	    public ResponseEntity<List<User>> viewAllUsers() {
+	        List<User> users = administratorService.viewAllUser();
+	        return new ResponseEntity<>(users, HttpStatus.OK);
+	    }
+	 
+	 @GetMapping("/viewAllHotelOwners")
+	    public ResponseEntity<List<HotelOwner>> viewAllHotelOwners() {
+	        List<HotelOwner> hotelOwners = administratorService.viewAllHotelOwner();
+	        return new ResponseEntity<>(hotelOwners, HttpStatus.OK);
+	    }
+	 
+	 
+	 @DeleteMapping("/manageRoomReservation/{reservationId}")
+	    public ResponseEntity<String> manageRoomReservation(@PathVariable Long reservationId,
+	                                                        @RequestParam(name = "reservationStatus") String reservationStatus)
+	            throws ReservationNotFoundException, InvalidCancellationException {
+	        administratorService.manageRoomReservation(reservationId, reservationStatus);
+	        return new ResponseEntity<>("Room reservation managed successfully", HttpStatus.OK);
+	    }
 
 }
--- Team 3 Project/cozyhaven/src/main/java/com/hexaware/ccozyhaven/restcontroller/HotelOwnerController.java
+++ Team 3 Project/cozyhaven/src/main/java/com/hexaware/ccozyhaven/restcontroller/HotelOwnerController.java
@@ -1,5 +1,78 @@
 package com.hexaware.ccozyhaven.restcontroller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hexaware.ccozyhaven.dto.HotelOwnerDTO;
+import com.hexaware.ccozyhaven.dto.RoomDTO;
+import com.hexaware.ccozyhaven.entities.HotelOwner;
+import com.hexaware.ccozyhaven.entities.Reservation;
+import com.hexaware.ccozyhaven.entities.Room;
+import com.hexaware.ccozyhaven.exceptions.HotelOwnerNotFoundException;
+import com.hexaware.ccozyhaven.exceptions.InvalidRefundException;
+import com.hexaware.ccozyhaven.exceptions.RefundProcessedException;
+import com.hexaware.ccozyhaven.exceptions.ReservationNotFoundException;
+import com.hexaware.ccozyhaven.exceptions.RoomNotFoundException;
+import com.hexaware.ccozyhaven.service.IHotelOwnerService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/cozyhaven-hotelowner")
 public class HotelOwnerController {
+	
+	 @Autowired
+	    private IHotelOwnerService hotelOwnerService;
+	 
+	 
+	 @PostMapping("/addrooms")
+	    public ResponseEntity<Room> addRoom(@RequestBody RoomDTO roomDTO) {
+	        Room addedRoom = hotelOwnerService.addRoom(roomDTO);
+	        return new ResponseEntity<>(addedRoom, HttpStatus.CREATED);
+	    }
+	 
+	 @PutMapping("/updateHotelOwner/{hotelOwnerId}")
+	    public ResponseEntity<HotelOwner> updateHotelOwner(@PathVariable Long hotelOwnerId, @RequestBody HotelOwnerDTO updatedHotelOwnerDTO)
+	            throws HotelOwnerNotFoundException {
+	        HotelOwner updatedHotelOwner = hotelOwnerService.updateHotelOwner(hotelOwnerId, updatedHotelOwnerDTO);
+	        return new ResponseEntity<>(updatedHotelOwner, HttpStatus.OK);
+	    }
+	 
+	 @PutMapping("/editRoom/{roomId}")
+	    public ResponseEntity<Room> editRoom(@PathVariable Long roomId, @RequestBody RoomDTO updatedRoomDTO)
+	            throws RoomNotFoundException {
+	        Room editedRoom = hotelOwnerService.editRoom(roomId, updatedRoomDTO);
+	        return new ResponseEntity<>(editedRoom, HttpStatus.OK);
+	    }
+	 
+	 @DeleteMapping("/removeRoom/{roomId}")
+	    public ResponseEntity<String> removeRoom(@PathVariable Long roomId) throws RoomNotFoundException {
+	        hotelOwnerService.removeRoom(roomId);
+	        return new ResponseEntity<>("Room removed successfully", HttpStatus.OK);
+	    }
+	 
+	 @GetMapping("/viewReservation/{hotelId}")
+	    public ResponseEntity<List<Reservation>> viewReservation(@PathVariable Long hotelId) {
+	        List<Reservation> reservations = hotelOwnerService.viewReservation(hotelId);
+	        return new ResponseEntity<>(reservations, HttpStatus.OK);
+	    }
+	 
+	 
+	 @GetMapping("/refundAmount/{reservationId}")
+	    public ResponseEntity<Double> refundAmount(@PathVariable Long reservationId)
+	            throws RefundProcessedException, InvalidRefundException, ReservationNotFoundException {
+	        Double refundedAmount = hotelOwnerService.refundAmount(reservationId);
+	        return new ResponseEntity<>(refundedAmount, HttpStatus.OK);
+	    }
 
 }
